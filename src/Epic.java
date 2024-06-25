afd0d47cb8596fecad88ddaaa188b1db270190a9
savde <savickiydmitriy94@gmail.com>
2024-06-26 01:12:15 +0700

    Добавлен функционал:Обновление задач, получение задачи по Id, удаление задачи по Id, получение списка подзадач определенного эпика

--- src/Epic.java
+++ src/Epic.java
@@ -3,7 +3,8 @@ import java.util.ArrayList;
 public class Epic extends Task {
     ArrayList<Integer> taskIds = new ArrayList<>();
 
-    public Epic(String name, String description) {
-        super(name, description);
+
+    public Epic(String name, String description, Status status) {
+        super(name, description, status);
     }
 }
