b82caa28e5f16eb60b4950b66e5438cf83190c9f
savde <savickiydmitriy94@gmail.com>
2024-06-25 23:46:37 +0700

    Добавлен функционал: add(Task ..);

--- src/Task.java
+++ src/Task.java
@@ -1,3 +1,21 @@
 public class Task {
     private int id;
+    private String name;
+    private String description;
+    private Status status;
+
+    public Task(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.status = Status.NEW;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,2 +1,22 @@
+import java.util.HashMap;
+
 public class TaskManager {
+
+    int nextId = 1;
+    HashMap<Integer, Task> tasks = new HashMap<>();
+    HashMap<Integer, Epic> epics = new HashMap<>();
+    HashMap<Integer, Subtask> subTasks = new HashMap<>();
+
+    public void add(Task task) {
+        task.setId(nextId++);
+        tasks.put(task.getId(), task);
+    }
+
+    public void add(Epic epic){
+
+    }
+
+    public void add(Subtask subtask){
+
+    }
 }
